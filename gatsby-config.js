73580380bab47433079240c1cd19b16ff6a170de
Matt Christians <christians.matt@gmail.com>
2020-03-12 08:30:07 -0500

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Matt Christians`,
+    description: `Frontend Software Engineer`,
+    author: `@matt_christians`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `matt-christians`,
+        short_name: `matt-christians`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
